9b920a2618eaaba9bb706dd90b90e6f288612e18
Mubariz Afzal <mubarizafzal@gmail.com>
2020-03-12 17:02:44 -0400

    added program details

--- Main.java
+++ Main.java
@@ -1,4 +1,22 @@
 
+/**
+ * This program is a simulator that shows how long the main hobo can survive jumping from track to track
+ * 
+ * The main hobo interacts with the following elements of the environment (in its attempt to survive):
+ * 	- Trains
+ * 		- determining timings for each track (negatively exponential)
+ * 	- Other hobos
+ * 		- message failure mechanism
+ * 		- delay in message after sending
+ * 		- chance of message being misread
+ * 	-  Variable S, fixed value of how long the main hobo stays on one track before moving
+ * 	- health and damage system
+ * 
+ * Learning rather than random jumping?
+ * 
+ * 
+ * 
+ */
 
 
 
@@ -7,6 +25,7 @@ class Main {
   public static void main (String args[]) {
   
     System.out.println("hellooo");
+    
   
   
   }
